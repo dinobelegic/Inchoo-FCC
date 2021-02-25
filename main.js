9f22a1cfeefcd50ed1908d954a180ec69f302521
Dino <dino.belegic@gmail.com>
2021-02-25 20:43:27 +0100

    Added functionality on homepage

---
+++ main.js
@@ -0,0 +1,32 @@
+// Show and hide search menu
+function showSearch() {
+    let show = document.getElementById("search"); 
+    show.classList.add("show-search"); 
+}
+
+function hideSearch() {
+    let hide = document.getElementById("search"); 
+    hide.classList.remove("show-search"); 
+}
+
+// Show and hide cart menu
+function showShoppingCart() {
+    let show = document.getElementById("shopping-cart"); 
+    show.classList.add("show-shopping-cart"); 
+}
+
+function hideShoppingCart() {
+    let hide = document.getElementById("shopping-cart"); 
+    hide.classList.remove("show-shopping-cart"); 
+}
+
+// Show and hide navigation
+function showNavigation() {
+    let show = document.getElementById("menu"); 
+    show.classList.add("show-menu"); 
+}
+
+function hideNavigation() {
+    let hide = document.getElementById("menu"); 
+    hide.classList.remove("show-menu"); 
+}
